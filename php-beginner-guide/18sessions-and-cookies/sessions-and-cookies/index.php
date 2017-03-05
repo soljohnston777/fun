f8e78816e9dcceda6cfddb948fc034d388d3ac32
Sol Johnston <sjohnston@basicengineeringpc.com>
2017-03-05 00:42:42 -0700

    update for cookies

--- php-beginner-guide/18sessions-and-cookies/sessions-and-cookies/index.php
+++ php-beginner-guide/18sessions-and-cookies/sessions-and-cookies/index.php
@@ -1,18 +1,8 @@
 <?php
-	//Sessions
-	session_start();
-	
-	if ( isset ( $_SESSION['views'] ) ) {
-		$_SESSION['views']++;
-	} else {
-		$_SESSION['views'] = 1;
-	}
-	
-	echo '<p>Pageviews: ' . $_SESSION['views'] . '</p>';
-	
-	session_destroy();
-	
-	//Cookies
+
+// What he didn't cover in this tutorial is that it is best to start with cookies then go to sessions for they can easily conflict with each other.
+
+	//Cookies  -  used on the users device
 	$expiry = time()+60*60*24;//1 day
 	setcookie('userdata[name]', 'John Morris', $expiry, '', '', '', TRUE);
 	setcookie('userdata[age]', '29', $expiry, '', '', '', TRUE);
@@ -25,4 +15,39 @@
 	echo '<pre>';
 	print_r($_COOKIE);
 	echo '</pre>'
+
+
+	//Sessions - used on the server (and more secure) - yet anything can be hacked it is a little harder
+	session_start();
+	
+	if ( isset ( $_SESSION['views'] ) ) {
+		$_SESSION['views']++;
+	} else {
+		$_SESSION['views'] = 1;
+	}
+	
+	echo '<p>Pageviews: ' . $_SESSION['views'] . '</p>';
+	
+	session_destroy();
+	
+	
+// DEFINE -More secure than sessions and recommend for security
+	
+define("CONSTANT", "Hello world.");
+echo CONSTANT; // outputs "Hello world."
+echo Constant; // outputs "Constant" and issues a notice.
+
+define("GREETING", "Hello you.", true);
+echo GREETING; // outputs "Hello you."
+echo Greeting; // outputs "Hello you."
+
+// Works as of PHP 7
+define('ANIMALS', array(
+    'dog',
+    'cat',
+    'bird'
+));
+echo ANIMALS[1]; // outputs "cat"
+
+	
 ?>
